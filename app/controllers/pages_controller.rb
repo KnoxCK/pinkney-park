b20f90bb7288d4c356d5f2a0b34c960c48ee0f88
Clarissa Knox <knox.ck@gmail.com>
2017-02-27 21:19:51 +0000

    landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,22 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :events, :stay, :properties, :contact, :local_area ]
 
   def home
   end
+
+  def events
+  end
+
+  def stay
+  end
+
+  def properties
+  end
+
+  def contact
+  end
+
+  def local_area
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'stay', to: 'pages#stay'
+  get 'events', to: 'pages#events'
+  get 'properties', to: 'pages#properties'
+  get 'contact', to: 'pages#contact'
+  get 'local_area', to: 'pages#local_area'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
