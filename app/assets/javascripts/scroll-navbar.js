f24f6c0fe50d795264b13c5d8f67be6584728850
Clarissa Knox <knox.ck@gmail.com>
2017-03-03 00:13:19 +0000

    nearly done

--- app/assets/javascripts/scroll-navbar.js
+++ app/assets/javascripts/scroll-navbar.js
@@ -1,7 +1,7 @@
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 
-    if (scroll > $(window).height() * 1.0) {
+    if (scroll > $(window).height() * 0.60) {
         $(".navbar-pinkneypark").addClass("hidden");
         $(".navbar-pinkneypark-scroll").removeClass("hidden");
     } else if (scroll < $(window).height()) {
