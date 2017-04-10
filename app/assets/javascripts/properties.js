80d9f4a9ed152bcaa60e3347239eb885a55ca147
Clarissa Knox <knox.ck@gmail.com>
2017-04-10 17:05:08 +0100

    changes

--- app/assets/javascripts/properties.js
+++ app/assets/javascripts/properties.js
@@ -9,3 +9,9 @@ $(document).on('click', '.residential-button', function(e) {
   $(".residential-page").removeClass("hidden");
 e.preventDefault();
 });
+
+$(document).on('click', '.resi-link', function(e) {
+  $(".commercial-page").addClass("hidden");
+  $(".residential-page").removeClass("hidden");
+e.preventDefault();
+});
