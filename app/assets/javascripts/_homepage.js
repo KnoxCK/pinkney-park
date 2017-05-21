b4691e48c14722653d9562780bcdd85b132588a2
Clarissa Knox <knox.ck@gmail.com>
2017-05-21 22:44:21 +0100

    gallery

--- app/assets/javascripts/_homepage.js
+++ app/assets/javascripts/_homepage.js
@@ -22,4 +22,18 @@ $(window).scroll(function() {
     }
 });
 
+function cycleMyImages(){
+    var $active = $('#banner-main-image .active');
+    var $next = ($active.next().length > 0) ? $active.next() : $('#banner-main-image img:first');
+    $next.css('z-index',5);//move the next image up the pile
+    $active.fadeOut(1500,function(){//fade out the top image
+	$active.css('z-index',-10).show().removeClass('active');//reset the z-index and unhide the image
+	$next.css('z-index',10).addClass('active');//make the next image the top one
+	});
+}
+
+$(document).ready(function(){
+// run every 7s
+setInterval('cycleMyImages()', 4000);
+})
 
