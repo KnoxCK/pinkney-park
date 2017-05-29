194103400e92dc16ec461c087de0d60497578795
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-05-29 15:38:30 +0100

    can't get it to work arghhhh

--- app/assets/javascripts/properties.js
+++ app/assets/javascripts/properties.js
@@ -1,14 +1,14 @@
-// $(document).on('click', '.commercial-button', function(e) {
-//   $(".commercial-page").removeClass("hidden");
-//   $(".residential-page").addClass("hidden");
-// e.preventDefault();
-// });
+$(document).on('click', '.commercial-button', function(e) {
+  $(".commercial-page").removeClass("hidden");
+  $(".residential-page").addClass("hidden");
+e.preventDefault();
+});
 
-// $(document).on('click', '.residential-button', function(e) {
-//   $(".commercial-page").addClass("hidden");
-//   $(".residential-page").removeClass("hidden");
-// e.preventDefault();
-// });
+$(document).on('click', '.residential-button', function(e) {
+  $(".commercial-page").addClass("hidden");
+  $(".residential-page").removeClass("hidden");
+e.preventDefault();
+});
 
 // $(document).on('click', '.resi-link', function(e) {
 //   $(".a[href$=properties").addClass("hidden");
@@ -19,7 +19,18 @@
 
 $(document).click('.residential-link', function(e){
   e.preventDefault();
-  console.log('oi');
-  $(".properties-buttons").tabs({active: 1});
+  window.location.href = "/properties#residential-button"
+  console.log('fuck');
+  var hash = document.location.hash;
+  if (hash) {
+    console.log($('.properties-buttons .'+hash+''));
+    $('.properties-buttons .'+hash+'').addClass('properties-button-active');
+    $(".commercial-page").addClass("hidden");
+    $(".residential-page").removeClass("hidden");
+
+  }
 });
+
+
+
 
