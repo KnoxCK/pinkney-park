8a14e9ffe8ad2d39574432584ae26e830f0854b6
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-05-29 13:33:21 +0100

    js fixes

--- app/assets/javascripts/_homepage.js
+++ app/assets/javascripts/_homepage.js
@@ -1,22 +1,19 @@
-$(window).scroll(function() {    
+$(window).scroll(function() {
     var scroll = $(window).scrollTop();
     if (scroll > $(window).height() * 0.30) {
         setTimeout(function() {
-	    $(".landing-header").removeClass("hidden");     
-		e.preventDefault();
+	    $(".landing-header").removeClass("hidden");
 		}, 300);
-        
+
         setTimeout(function() {
 		$(".landing-para").removeClass("hidden");
-		e.preventDefault();
 		}, 700);
 
 		setTimeout(function() {
 		$(".landing-boxes-all").removeClass("hidden");
 		$(".landing-margin").addClass("hidden");
-		e.preventDefault();
 		}, 1100);
-    } 
+    }
     else if (scroll < $(window).height()) {
     }
 });
