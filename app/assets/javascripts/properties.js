b98e0b6146f64bffebb8a4b846333fd0c2ebd1ec
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-05-29 16:33:51 +0100

    logo file names changed

--- app/assets/javascripts/properties.js
+++ app/assets/javascripts/properties.js
@@ -10,13 +10,6 @@ $(document).on('click', '.residential-button', function(e) {
 e.preventDefault();
 });
 
-// $(document).on('click', '.resi-link', function(e) {
-//   $(".a[href$=properties").addClass("hidden");
-//   $(".commercial-page").addClass("hidden");
-//   $(".residential-page").removeClass("hidden");
-// e.preventDefault();
-// });
-
 $(document).ready(function(){
   var hash = window.location.hash
   console.log(hash);
