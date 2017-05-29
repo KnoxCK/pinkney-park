bd716307cf6f4ff298494d5bc05b6d8d575d0d1a
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-05-29 16:35:01 +0100

    js cleaned up

--- app/assets/javascripts/properties.js
+++ app/assets/javascripts/properties.js
@@ -12,9 +12,7 @@ e.preventDefault();
 
 $(document).ready(function(){
   var hash = window.location.hash
-  console.log(hash);
   if (hash) {
-    console.log($('.properties-buttons '+hash+''));
     $('.properties-buttons '+hash+'').addClass('properties-button-active');
     $(".commercial-page").addClass("hidden");
     $(".residential-page").removeClass("hidden");
